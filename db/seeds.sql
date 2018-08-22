556fe7c7d477d45ca8864df7297bff66a23934ca
shellyrgarner <sgrenea@gmail.com>
2018-08-21 20:41:26 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers (id, burger_name, devoured) VALUES (veggie_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (bacon_cheeseburger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (cali_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (vegan_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (steak_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (lamb_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (boring_burger, false);
-INSERT INTO burgers (id, burger_name, devoured) VALUES (deer_burger, false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon_cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cali_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegan_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('steak_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('lamb_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('boring_burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('deer_burger', false);
